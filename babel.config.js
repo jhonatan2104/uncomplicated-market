54d9691acdb6646beb1663fc18e747f6434dc2a7
jhonatan2104 <xdjhonatan0@gmail.com>
2021-01-11 23:27:58 -0300

    chore: add eslint

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: [
+            '.ts',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json'
+          ],
+          alias: {
+            '@': './src',
+            '~': '.'
+          }
+        }
+      ]
+    ]
+  }
+}
