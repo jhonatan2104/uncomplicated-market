f9b1d78f4518c854329edc2fee224e6eeae7cb79
jhonatan2104 <xdjhonatan0@gmail.com>
2021-01-14 07:25:21 -0300

    refactor: adjusts usecase verify access code to new model account

--- src/data/usecases/company/verify-access-code/fire-verify-access-code.ts
+++ src/data/usecases/company/verify-access-code/fire-verify-access-code.ts
@@ -1,18 +1,18 @@
 import { VerifyAccessCode } from '@/domain/usecases/company/verify-access-code'
 import { FireClient } from '@/infra'
-import { COMPANY } from '@/infra/constants/collections'
+import { ACCOUNT } from '@/infra/constants/collections'
 
 export class FireVerifyAccessCode extends FireClient implements VerifyAccessCode {
   execute = async (params: VerifyAccessCode.Params): Promise<boolean> => {
     try {
-      const companyResult = await this.db.collection(COMPANY).doc(params.cnpj).get()
+      const accountQuerySnapshot = await this.db.collection(ACCOUNT).where('type.cnpj', '==', params.cnpj).get()
 
-      if (!companyResult.exists) {
+      if (accountQuerySnapshot.empty) {
         throw new Error(`A empresa com o cnpj ${params.cnpj} não existe`)
       } else {
-        const company = companyResult.data()
+        const account = accountQuerySnapshot.docs[0].data()
 
-        return company?.accessCode === params.accessCode
+        return account?.type?.accessCode === params.accessCode
       }
     } catch (e) {
       throw new Error(e)
