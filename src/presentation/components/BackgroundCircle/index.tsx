f0308136d85cd22b263387c7c6a5d15ce9249292
jhonatan2104 <xdjhonatan0@gmail.com>
2021-01-18 21:09:58 -0300

    feat: add background circle

---
+++ src/presentation/components/BackgroundCircle/index.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { View, ViewProps, ScrollView, StyleSheet, SafeAreaView, Text } from 'react-native'
+import Constants from 'expo-constants'
+
+type Props = {
+  bg: string
+  children: React.ReactNode
+  bgBack?: string
+}
+
+const styles = StyleSheet.create({
+  scroll: {
+    flex: 1,
+    width: 658
+  },
+  containerScroll: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 16
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: 658
+  },
+  circle: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 658,
+    alignSelf: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+    alignItems: 'center'
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    alignSelf: 'center'
+  }
+})
+
+const BackgroundCircle: React.FC<ViewProps & Props> = (props: ViewProps & Props) => {
+  const { children, bg, bgBack } = props
+
+  return (
+    <SafeAreaView style={{ backgroundColor: '#fff', flex: 1, marginTop: Constants.statusBarHeight }}>
+      <ScrollView
+        style={styles.scroll}
+        contentContainerStyle={styles.containerScroll}
+      >
+        <View style={[
+          styles.container,
+          {
+            backgroundColor: bgBack ?? '#fff'
+          }
+
+        ]}>
+          <View style={[
+            styles.circle,
+            {
+              backgroundColor: bg
+            }
+          ]}>
+            {children}
+          </View>
+        </View>
+      </ScrollView>
+      <View style={styles.footer}>
+        <Text style={{
+          fontFamily: 'Roboto-Regular',
+          fontSize: 23
+        }}>v 1.0.0</Text>
+      </View>
+    </SafeAreaView>
+
+  )
+}
+
+export default BackgroundCircle
--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,1 +1,2 @@
 export { default as ButtonCircular } from './ButtonCircular'
+export { default as BackgroundCircle } from './BackgroundCircle'
