3d35525eeea0b340292d6115c94ce0072f406343
jhonatan2104 <xdjhonatan0@gmail.com>
2021-01-31 18:28:14 -0300

    feat: add color of texts

--- src/presentation/theme/index.ts
+++ src/presentation/theme/index.ts
@@ -2,7 +2,9 @@ const colorsBase = {
   darkBlue: '#050517',
   orange: '#FE5F55',
   white: '#fff',
-  darkWhite: '#F4F5F7'
+  darkWhite: '#F4F5F7',
+  gray: '#ccc',
+  blackLight: '#333'
 }
 
 export default {
@@ -11,13 +13,17 @@ export default {
     primary: colorsBase.darkBlue,
     second: colorsBase.orange,
     textLight: colorsBase.white,
-    base: colorsBase.darkWhite
+    textDark: colorsBase.blackLight,
+    base: colorsBase.darkWhite,
+    opaque: colorsBase.gray
   },
   cooperator: {
     primary: colorsBase.orange,
     second: colorsBase.darkBlue,
     textLight: colorsBase.white,
-    base: colorsBase.darkWhite
+    textDark: colorsBase.blackLight,
+    base: colorsBase.darkWhite,
+    opaque: colorsBase.gray
   }
 }
 
@@ -27,9 +33,13 @@ export type Theme = {
     orange: string
     white: string
     darkWhite: string
+    gray: string
+    blackLight: string
   }
   primary: string
   second: string
   textLight: string
   base: string
+  opaque: string
+  textDark: string
 }
